7012be8bb1664e3edd339cecfc4f092ac371e2b1
Kola Teja Naga Sri <143061461+TejaNagaSri13@users.noreply.github.com>
2025-12-29 14:01:15 +0530

    Update Recipe.js

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose');
 
-const RecipeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    ingredients: { type: [String], required: true },
-    instructions: { type: String, required: true },
-});
+const RecipeSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        ingredients: {
+            type: [String],
+            required: true
+        },
+        instructions: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
 
 module.exports = mongoose.model('Recipe', RecipeSchema);
