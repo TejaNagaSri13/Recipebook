a79160f1e2215a77ab232c12f0bdcf76092bbced
Kola Teja Naga Sri <143061461+TejaNagaSri13@users.noreply.github.com>
2025-12-29 14:51:59 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import "./Navbar.css"; 
-
+import "./Navbar.css";
 
 const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
-  const [cookies, setCookies, removeCookies] = useCookies(["access_token"]);
+  const [, , removeCookies] = useCookies(["token"]);
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    removeCookies("access_token");
+    removeCookies("token", { path: "/" });
     localStorage.removeItem("username");
+    localStorage.removeItem("userID");
     setIsLoggedIn(false);
     navigate("/");
   };
 
   return (
     <nav className="navbar">
-      <h1>PakkaSwad</h1>
+      <h1>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          PakkaSwad
+        </Link>
+      </h1>
+
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/create-recipe">Create Recipe</Link></li>
+
+        {isLoggedIn && (
+          <li><Link to="/create-recipe">Create Recipe</Link></li>
+        )}
+
         <li><Link to="/contact">Contact</Link></li>
 
         {!isLoggedIn ? (
